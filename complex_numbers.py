00b86e38888de5682b7d70dd05e75d886bd434b9
Jarod Rosenthal <40376569+JarodRosenthal@users.noreply.github.com>
2021-10-24 22:37:35 -0400

    Update complex_numbers.py

--- complex_numbers.py
+++ complex_numbers.py
@@ -1,69 +1,55 @@
-#!/usr/bin/env python
-
 import math
-
 class Complex:
-    def __init__(self, real, imag):
-        self.real = real
-        self.imag = imag
-    
-    def __str__(self):
-        ret = ""
-        real_str = "%.2f" % self.real
-        imag_str = "%.2f" % self.imag
-        if self.real > 0 or self.real < 0:
-            ret += real_str
-        if self.imag > 0 or self.imag < 0:
-            if self.imag > 0:
-                if ret != "":
-                    ret += " + " + imag_str + "i"
-                else:
-                    ret += imag_str + "i"
-            else:
-                if ret != "":
-                    new_imag = -1 * self.imag
-                    imag_str = "%.2f" % new_imag
-                    ret += " - " + imag_str + "i"
-                else:
-                    ret += imag_str + "i"
-        if ret == "":
-            ret += real_str
-        return ret
-    
-    def __add__(self, other):       
-        new_real = self.real + other.real
-        new_imag = self.imag + other.imag
-        new_complex = Complex(new_real,new_imag)
-        return str(new_complex)
-    def __sub__(self, other):
-        new_real = self.real - other.real
-        new_imag = self.imag - other.imag
-        new_complex = Complex(new_real,new_imag)
-        return str(new_complex)
-    def __mul__(self, other):
-        new_real = (self.real * other.real) - (self.imag * other.imag)
-        new_imag = (self.real * other.imag) + (self.imag * other.real)
-        new_complex = Complex(new_real,new_imag)
-        return str(new_complex)
-    def __div__(self, other):
-        divisor = (other.real ** 2) + (other.imag ** 2)
-        new_real = ((self.real * other.real) + (self.imag * other.imag)) / divisor
-        new_imag = ((self.imag * other.real) - (self.real * other.imag)) / divisor
-        new_complex = Complex(new_real,new_imag)
-        return str(new_complex)
-    def mod(self):
-        new_real = math.sqrt(self.real ** 2 + self.imag ** 2)
-        new_complex = Complex(new_real, 0.0)
-        return str(new_complex)
+    def __init__(self, a, b):
+        self.a, self.b = a, b;
+    def display(self):
+        if self.a < 0.005 and self.a > -0.005:
+            self.a = 0;
+        if self.b < 0.005 and self.b > -0.005:
+            self.b = 0;
+        if self.a != 0:
+            str1 = '%0.2f' % self.a;
+            if self.b > 0:
+                str1 += ' + %0.2fi' % self.b;
+            elif self.b < 0:
+                str1 += ' - %0.2fi' % -self.b;
+        elif self.b != 0:
+            str1 = '%0.2fi' % self.b;
+        else:
+            str1 = '0.00';
+        print str1;
+        
+    def conjugate(self):
+        return Complex(self.a, -self.b);
+    def norm(self):
+        return self.a*self.a + self.b*self.b;
+    def scale(self, scalar):
+        return Complex(self.a*scalar, self.b*scalar);
 
-a = [float(x) for x in raw_input().split(' ')]
-b = [float(x) for x in raw_input().split(' ')]
-ca = Complex(a[0],a[1])
-cb = Complex(b[0],b[1])
-print ca + cb
-print ca - cb 
-print ca * cb
-print ca / cb
-print ca.mod()
-print cb.mod()
+    
+def add(a, b):
+    return Complex(a.a + b.a, a.b + b.b);
+def sub(a, b):
+    return Complex(a.a - b.a, a.b - b.b);
+def mul(a, b):
+    return Complex(a.a * b.a - a.b * b.b, a.a * b.b + a.b * b.a);
+def div(a, b):
+    return mul(a, b.conjugate().scale(1.0/b.norm()));
+    
+x = Complex(0, 0);
+y = Complex(0, 0);
 
+[a, b] = raw_input().split();
+x.a = float(a);
+x.b = float(b);
+
+[a, b] = raw_input().split();
+y.a = float(a);
+y.b = float(b);
+
+add(x, y).display();
+sub(x, y).display();
+mul(x, y).display();
+div(x, y).display();
+print '%0.2f' % math.sqrt(x.norm());
+print '%0.2f' % math.sqrt(y.norm());
